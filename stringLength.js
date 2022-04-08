7bb7b15c148bd7edc30a5ad8df825a176f97d8fd
maen <maen.1980@yahoo.com>
2022-04-08 05:19:30 +0300

    string length function add 1-10 condition and pass the test

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,5 @@
 function stringLength(str) {
-    return str.length;
+    return str.length > 0 && str.length < 11 ? str.length : 'string should be between 1 and 10 length';
 }
 
 module.exports = stringLength;
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,9 +1,13 @@
 const stringLength = require('./stringLength.js');
 
 it('Work', () => {
-    expect(stringLength('Hello World')).toBe(11);
+    expect(stringLength('Hello World')).toBe('string should be between 1 and 10 length');
 });
 
 it('Work', () => {
     expect(stringLength('maen')).toBe(4);
+});
+
+it('Work', () => {
+    expect(stringLength('maen mohammad')).toBe('string should be between 1 and 10 length');
 });
